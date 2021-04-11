dbd1d71fd229dfa24e94adb2fba0ce067fe4b0f9
8aleph <brett.a.tompkins@gmail.com>
2021-04-11 09:34:19 -0700

    rename init function in ccc script

--- web/config/scripts/clear-container-cache.sh
+++ web/config/scripts/clear-container-cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ls web/code/cache/container* 2> /dev/null | while read file; do
     if [ -f "${file}" ]; then
-        init-docker-compose-run rm "$file"
+        initDockerComposeRun rm "$file"
     fi
 done
 
